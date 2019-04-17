5cb0ecf441a8b60cf04610a6da26879d28802ecd
therealyingtong <yingtong@ethereum.org>
2019-04-17 13:57:42 +0800

    add ropsten deployment

--- truffle.js
+++ truffle.js
@@ -1,3 +1,10 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const infuraKey = 'ce26c7d418db4e4695aaa359a1013ab2';
+// const private = require('./private.json');
+// const mnemonic = private.funds;
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -6,6 +13,15 @@ module.exports = {
       host : "localhost",
       port: 8545,
       network_id: "*", // match any network
+    },
+
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
+      network_id: '*',
+      gas: 7000000,
+      gasPrice: 3000000000, // 50 gwei, this is very high
+      skipDryRun: true,
+
     }
   }
 };
